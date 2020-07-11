6a872a0d13fb84803d8e36ed237f290f97d93df2
Jamsheer <pkjams@gmail.com>
2020-07-11 20:29:19 +0800

    some minor updates

--- pkjams.com/src/App.js
+++ pkjams.com/src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Jamsheer - Coming soon.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://pkjams.github.io"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Know more
         </a>
       </header>
     </div>
